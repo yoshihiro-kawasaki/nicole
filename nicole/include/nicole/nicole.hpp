e18b671065a2229e72b5635e30d55996e3d876ed
yoshihiro-kawasaki <kawayoshi.0117@gmail.com>
2026-03-22 13:20:05 +0900

    nicole.hppの追加

--- nicole/include/nicole/nicole.hpp
+++ nicole/include/nicole/nicole.hpp
@@ -1,49 +1,22 @@
-/**
- * @file nicole.hpp
- * @brief NICOLE: A framework for computing chemical reactions and non-ideal MHD resistivity in astrophysics.
- * 
- * This header provides access to all major modules of the NICOLE framework.
- * 
- * @date 2025-02-11
- * @author Y. Kawasaki
- */
+#ifndef NICOLE_HPP_
+#define NICOLE_HPP_
 
-#ifndef NICOLE_HPP
-#define NICOLE_HPP
+// config
+#include "nicole/config/input_config.hpp"
 
 // Manages chemical elements
-#include "elements/element_manager.hpp"
+#include "nicole/element/element_manager.hpp"
 
 // Manages chemical species
-#include "species/species_manager.hpp"
+#include "nicole/species/species_manager.hpp"
 
-// Manages chemical reactions
-#include "reactions/reaction_manager.hpp"
+// // Manages chemical reactions
+// #include "reactions/reaction_manager.hpp"
 
-// Runs chemical reaction simulations
-#include "reaction_simulator/reaction_simulator.hpp"
+// // Runs chemical reaction simulations
+// #include "reaction_simulator/reaction_simulator.hpp"
 
-// Computes non-ideal MHD resistivity
-#include "mhd_resistivity/non_ideal_mhd_effect.hpp"
-
-// namespace nicole {
-
-//     class Nicole
-//     {
-//     public:
-//         Nicole(InputConfig& input);
-//         Nicole(InputConfig& input, const std::vector<std::string>& user_gas_species_list);
-
-//     private:
-
-//         ElementManager element_manager_;
-//         SpeciesManager species_manager_;
-//         ReactionManager reaction_manager_;
-//         ReactionSimulator reaction_simulator_;
-//         NonIdealMHDeffect non_ideal_mhd_effect_;
-
-//         bool is_check_;
-//     };
-// }
+// // Computes non-ideal MHD resistivity
+// #include "mhd_resistivity/non_ideal_mhd_effect.hpp"
 
 #endif /* NICOLE_HPP */
